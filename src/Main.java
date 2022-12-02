1f7be2347c29941fb5720a0d666fdec5b56e802d
Владислав Маслов <maslov-vd.inc@msk.elara.ru>
2022-12-02 13:38:26 +0300

    Проверочный

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 20 ;
+        int coast = 1200;
+        int y = coast/x;
+        System.out.println( "Начислено " + y + " миль(и)");
+    }
+}
